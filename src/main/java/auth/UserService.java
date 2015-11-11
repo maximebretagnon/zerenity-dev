ffb090b1c574ee204a2fbfc3c6c1a3595eaff07c
Florent Guillaume <guillaume.florent@live.fr>
2015-11-11 21:25:39 +0100

    delete auth + Main

--- src/main/java/auth/UserService.java
+++
@@ -1,9 +0,0 @@
-package auth;
-
-import domain.User;
-
-public interface UserService {
-    User findUser(String username, String passwd);
-    User findUser(long id);
-    void syncUser(User user);
-}
