f6e9be02ddabcfab7330887ffaa71856b16ba281
alexLerb <al.31@live.fr>
2015-11-12 11:04:29 +0100

    DemoHibernate

---
+++ src/main/java/demohibernate/DemoHibernate.java
@@ -0,0 +1,32 @@
+package demohibernate;
+
+import domain.Room;
+import persistence.HibernateUtil;
+
+public class DemoHibernate {
+	
+	public static void main (String[] args){
+		
+		// 1: ouverture unité de travail hibernate
+		Session session = HibernateUtil.currentSession();
+		
+		//2: ouverture transaction
+		Transaction tx = session.beginTransaction();
+		
+		//3:instanciation objet métier
+		Room room = new Room();
+
+		//4:création d'un objet en base
+		room.setCapacity((short) 30);
+		room.setRoomArea("10 m");
+		room.setRoomType("office");
+		session.save(room);
+		
+		//5:fermeture transaction
+		tx.commit();
+		
+		//6:fermeture unité de travail hibernate
+		HibernateUtil.closeSession();
+	}
+
+}
