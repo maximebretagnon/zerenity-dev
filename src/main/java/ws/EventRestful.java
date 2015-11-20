0c399b0242884d16f15409fb1b6f34b14a06c29c
Maxime B <maxb.facebook@gmail.com>
2015-11-20 11:00:46 +0100

    Revert "Add global CORS"
    This reverts commit 7575699ceef0a7da466040a3bdefd4af7555b860.

--- src/main/java/ws/EventRestful.java
+++ src/main/java/ws/EventRestful.java
@@ -179,7 +179,6 @@ public class EventRestful {
 		return Response.ok()
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD")
-				.header("Access-Control-Allow-Credentials", "true")
 				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, access-control-allow-origin")
 				.build();
 	}
@@ -190,7 +189,6 @@ public class EventRestful {
 	    return Response.ok()
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD")
-				.header("Access-Control-Allow-Credentials", "true")
 				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, access-control-allow-origin, auth-token, auth-username")
 				.build();
 	}
